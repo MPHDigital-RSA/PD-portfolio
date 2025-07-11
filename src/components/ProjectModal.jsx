d07ac9b54a6b324c86cc75454e0f63b8489bae35
Prince Hlela <mphdigital@protonmail.com>
2025-07-11 17:29:56 +0200

    update the modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,12 +1,13 @@
 import { FaGithub } from "react-icons/fa";
 import { AiOutlineLink } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 
 function ProjectModal({ project, setIsProjectModal }) {
     return (
-        <div className='max-w-[600px] w-full bg-[white] dark:bg-[var(--color-black)] space-y-4 p-5 rounded'>
-            <button className="bg-[var(--color-black)] w-fit text-white px-5 py-2 rounded cursor-pointer dark:bg-white dark:text-[var(--color-black)]" onClick={() => setIsProjectModal(false)}>close</button>
+        <div className='max-w-[600px] w-full bg-[white] dark:bg-[var(--color-black)] space-y-4 p-5 rounded flex flex-col'>
+            <button className="bg-[var(--color-black)] w-fit text-white p-3 rounded cursor-pointer dark:bg-white dark:text-[var(--color-black)] mx-auto" onClick={() => setIsProjectModal(false)}><AiOutlineClose /></button>
             <img src={project.image} alt={`${project.title} image`} />
-            <div className="text-2xl flex space-x-4 items-center justify-center">
+            <div className="text-2xl flex space-x-4 items-center justify-center text-[var(--color-black)] dark:text-white">
                 <a href={project.github} target="_blank" onClick={() => setIsProjectModal(false)}>
                     <FaGithub />
                 </a>
